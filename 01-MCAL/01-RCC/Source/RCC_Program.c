155a1ca804c6d757e1c7cacc593aaa0d7b1257f2
Mohamed Abdo Maklad <mohamedabdo75200@gmail.com>
2023-08-01 04:24:08 +0300

    RCC_V0_Update

--- 01-MCAL/01-RCC/Source/RCC_Program.c
+++ 01-MCAL/01-RCC/Source/RCC_Program.c
@@ -102,10 +102,9 @@ void RCC_voidInitSysClk(SystemClockSource_t Clock_Source, CSS_State_t Copy_CSSSt
     case PLL_HSE:
         RCC_CC.RC.HSEON = STD_High;
         RCC_CC.RC.PLLON = STD_High;
-        RCC_CC.PLLCFGR.PLLSRC = (Clock_Source>>1);
+        RCC_CC.PLLCFGR.PLLSRC = (Clock_Source - 1);
         break;
     };
-    RCC_CC.CFGR.SW_rw = Clock_Source;
     RCC_CC.RC.CSSON = Copy_CSSState;
 }
 /**************************************************************************************/
