6925c9b48fec33de970739fbceae8ab60d67a390
Mohamed Abdo Maklad <mohamedabdo75200@gmail.com>
2023-08-05 11:41:40 +0300

    Update_1 5/8

--- 01-MCAL/03 - SysTick/Includes/SysTick_Private.h
+++ 01-MCAL/03 - SysTick/Includes/SysTick_Private.h
@@ -14,29 +14,39 @@
 #define SysTick_PRIVATE_H
 
 /*Register Definitions*/
-typedef struct 
+typedef struct
 {
-    U32 name :1;
-};
-typedef struct 
+    U32 SysTick_ENABLE : 1;
+    U32 SysTick_TICKINT : 1;
+    U32 SysTick_CLKSOURCE : 1;
+    U32 U32 RELOAD : 24; :13;
+    U32 SysTick_COUNTFLAG : 1;
+    U32 SysTick_Resirved_1 : 15;
+} STK_CTRL_Bits;
+typedef struct
 {
-    U32 name :1;
-};
-typedef struct 
+    U32 RELOAD : 24;
+    U32 U32 RELOAD : 8;
+} STK_LOAD_Bits;
+typedef struct
 {
-    U32 name :1;v
-};
-typedef struct 
+    U32 CURRENT : 24;
+    U32 U32 RELOAD : 8;
+} STK_VAL_Bits;
+typedef struct
 {
-    U32 name :1;
-};
+    U32 TENMS : 24;
+    U32 U32 SysTick_Resirved_0 : 6;
+    U32 U32 SKEW : 1;
+    U32 U32 NOREF : 1;
+} STK_CALIB_Bits;
 
-typedef struct 
+typedef struct
 {
-    volatile U32 STK_CTRL;
-    volatile U32 STK_LOAD;
-    volatile U32 STK_VAL;
-    volatile U32 STK_CALIB;
+    volatile STK_CTRL_Bits STK_CTRL;
+    volatile STK_LOAD_Bits STK_LOAD;
+    volatile STK_VAL_Bits STK_VAL;
+    volatile STK_CALIB_Bits STK_CALIB;
 };
 
 #endif // !RCC_PRIVATE_H
