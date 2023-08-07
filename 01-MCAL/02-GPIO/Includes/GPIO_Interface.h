1c617e56bab349a020cd70010dfe3a4c49e6649b
Mohamed Abdo Maklad <mohamedabdo75200@gmail.com>
2023-08-07 11:52:37 +0300

    GPIO Driver Preparation

--- 01-MCAL/02-GPIO/Includes/GPIO_Interface.h
+++ 01-MCAL/02-GPIO/Includes/GPIO_Interface.h
@@ -13,4 +13,90 @@
 #ifndef GPIO_INTERFACE_H
 #define GPIO_INTERFACE_H
 
-#endif // !RCC_INTERFACE_H
+typedef enum
+{
+    // Port A (0 To 15)
+    GPIO_A0 = 0,
+    GPIO_A1,
+    GPIO_A2,
+    GPIO_A3,
+    GPIO_A4,
+    GPIO_A5,
+    GPIO_A6,
+    GPIO_A7,
+    GPIO_A8,
+    GPIO_A9,
+    GPIO_A10,
+    GPIO_A11,
+    GPIO_A12,
+    GPIO_A13,
+    GPIO_A14,
+    GPIO_A15,
+    // Port B (0 To 15)
+    GPIO_B0,
+    GPIO_B1,
+    GPIO_B2,
+    GPIO_B3,
+    GPIO_B4,
+    GPIO_B5,
+    GPIO_B6,
+    GPIO_B7,
+    GPIO_B8,
+    GPIO_B9,
+    GPIO_B10,
+    GPIO_B11,
+    GPIO_B12,
+    GPIO_B13,
+    GPIO_B14,
+    GPIO_B15,
+    // Port C (0 To 2)
+    GPIO_C13,
+    GPIO_C14,
+    GPIO_C15
+} GPIO_PINS_t;
+typedef enum
+{
+    GPIO_PortA = 0,
+    GPIO_PortB,
+    GPIO_PortC
+} GPIO_PORT_t;
+typedef enum
+{
+    GPIO_INPUT = 0,
+    GPIO_OUTPUT,
+    GPIO_AltFunc,
+    GPIO_ANALOG
+} GPIO_PinMode_t;
+typedef enum
+{
+    GPIO_FLOATING = 0,
+    GPIO_PullUp,
+    GPIO_PullDown
+} GPIO_InputPinMode_t;
+typedef enum
+{
+    GPIO_PushPull = 0,
+    GPIO_OpenDrain
+} GPIO_OutputPinMode_t;
+typedef enum
+{
+    GPIO_Output_LS = 0, // 8MHZ
+    GPIO_Output_MS,     // 50MHz
+    GPIO_Output_HS      // 80MHz
+    // GPIO_Output_VHS     // 150MHz Not USed For STM32F401CCU6
+} GPIO_OutputPinSpeed_t;
+typedef enum
+{
+    GPIO_LOW = 0,
+    GPIO_HIGH
+} GPIO_PinValue_t;
+void GPIO_voidSetPinMode(GPIO_PINS_t Copy_PinID, GPIO_PinMode_t Copy_PinMode);
+void GPIO_voidInputPinConfig(GPIO_InputPinMode_t Copy_InputPinMode);
+void GPIO_voidOutputPinConfig(GPIO_OutputPinMode_t Copy_OutputPinMode, GPIO_OutputPinSpeed_t Copy_OutputPinSpeed);
+void GPIO_voidSetPinValue(GPIO_PinValue_t Copy_PinValue);
+void GPIO_voidSetPinValueAtomicly(GPIO_PinValue_t Copy_PinValue);
+U8 GPIO_U8GetPinValue(GPIO_PINS_t Copy_PinID);
+void GPIO_SetPortValue(GPIO_PORT_t Copy_PortID, U16 Copy_Value);
+void GPIO_SetPortValueAtomicly(GPIO_PORT_t Copy_PortID, U16 Copy_Value);
+
+#endif // !GPIO_INTERFACE_H
--- 01-MCAL/02-GPIO/Includes/GPIO_Private.h
+++ 01-MCAL/02-GPIO/Includes/GPIO_Private.h
@@ -14,30 +14,26 @@
 #define GPIO_PRIVATE_H
 
 /*Register Definitions*/
-#define RCC_BoundaryAddress 0x40021000
-#define RCC_RC_Offset       0x00
-#define RCC_CFGR_Offset     0x04
-#define RCC_CIR_Offset      0x08
-#define RCC_APB2RSTR_Offset 0x0C
-#define RCC_APB1RSTR_Offset 0x010
-#define RCC_AHBENR_Offset   0x14
-#define RCC_APB2ENR_Offset  0x18
-#define RCC_APB1ENR_Offset  0x1C
-#define RCC_BDCR_Offset     0x20
-#define RCC_CSR_Offset      0x24
 
-#define RCC_RC       (*((volatile U32 *)(RCC_BoundaryAddress + RCC_RC_Offset      )))           //clock
-#define RCC_CFGR     (*((volatile U32 *)(RCC_BoundaryAddress + RCC_CFGR_Offset    )))           //clock
-#define RCC_CIR      (*((volatile U32 *)(RCC_BoundaryAddress + RCC_CIR_Offset     )))  //reset
-#define RCC_APB2RSTR (*((volatile U32 *)(RCC_BoundaryAddress + RCC_APB2RSTR_Offset)))  //reset
-#define RCC_APB1RSTR (*((volatile U32 *)(RCC_BoundaryAddress + RCC_APB1RSTR_Offset)))  //reset
-#define RCC_AHBENR   (*((volatile U32 *)(RCC_BoundaryAddress + RCC_AHBENR_Offset  )))           //clock AHB
-#define RCC_APB2ENR  (*((volatile U32 *)(RCC_BoundaryAddress + RCC_APB2ENR_Offset )))           //clock APB2
-#define RCC_APB1ENR  (*((volatile U32 *)(RCC_BoundaryAddress + RCC_APB1ENR_Offset )))           //clock APB1
-#define RCC_BDCR     (*((volatile U32 *)(RCC_BoundaryAddress + RCC_BDCR_Offset    )))  //reset
-#define RCC_CSR      (*((volatile U32 *)(RCC_BoundaryAddress + RCC_CSR_Offset     )))  //reset
+typedef struct
+{
+    volatile U32 MODER;
+    volatile U32 OTYPER;
+    volatile U32 OSPEEDR;
+    volatile U32 PUPDR;
+    volatile U32 IDR;
+    volatile U32 ODR;
+    volatile U32 BSRR;
+    volatile U32 LCKR;
+    volatile U32 AFRL;
+    volatile U32 AFRH;
+} GPIO_t;
+#define GPIOA_BoundaryAddress 0x40020000
+#define GPIOB_BoundaryAddress 0x40020400
+#define GPIOC_BoundaryAddress 0x40020800
 
-
-
+#define GPIOA (*((volatile GPIO_t *)GPIOA_BoundaryAddress))
+#define GPIOB (*((volatile GPIO_t *)GPIOB_BoundaryAddress))
+#define GPIOC (*((volatile GPIO_t *)GPIOC_BoundaryAddress))
 
 #endif // !RCC_PRIVATE_H
