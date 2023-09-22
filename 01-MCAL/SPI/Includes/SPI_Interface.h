f4854e0602614c44a704582cfcead7973a6311fc
Mohamed Abdo Maklad <mohamedabdo75200@gmail.com>
2023-09-22 11:50:01 +0300

    SPI Driver Files

---
+++ 01-MCAL/SPI/Includes/SPI_Interface.h
@@ -0,0 +1,19 @@
+/****************************************************/
+/*Author	            : Mohamad Abdo Maklad	    */
+/*Date		            : 27-7-2023				    */
+/*Last Modification		: 27-7-2023				    */
+/*Version	            : V01				   	    */
+/****************************************************/
+/*Controller            :                           */
+/*SWC                   :                           */
+/*Description           :                           */
+/****************************************************/
+
+/*File Gard*/
+#ifndef SPI_INTERFACE_H
+#define SPI_INTERFACE_H
+/*Options*/
+
+/*functions Prototypes*/
+
+#endif // !SWC_INTERFACE_H
---
+++ 01-MCAL/SPI/Source/SPI_Program.c
@@ -0,0 +1,19 @@
+/****************************************************/
+/*Author	            : Mohamad Abdo Maklad	    */
+/*Date		            : 27-7-2023				    */
+/*Last Modification		: 27-7-2023				    */
+/*Version	            : V01				   	    */
+/****************************************************/
+/*Controller            :                           */
+/*SWC                   :                           */
+/*Description           :                           */
+/****************************************************/
+/*LIB Includes*/
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+/*Lower Layer Includes*/
+/*Driver Files Includes*/
+#include "SPI_Interface.h"
+#include "SPI_Private.h"
+#include "SPI_Config.h"
+/*Driver*/
